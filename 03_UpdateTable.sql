c61279421594685e0241099e62f8d5c1b13f25aa
monica-tsa <monicasilva8853@gmail.com>
2025-12-01 06:06:14 -0300

    Criando update daas tabelas do minimundo

---
+++ 03_UpdateTable.sql
@@ -0,0 +1,21 @@
+---UPDATE----
+
+----ATUALIZAR O TELEFONE DO CLIENTE----
+
+UPDATE cliente 
+SET telefone = '11911112222' 
+WHERE id_cliente = 1;
+
+
+----ATUALIZAR O PRECO DE UM PRODUTO----
+
+UPDATE produto 
+SET preco = 49.90 
+WHERE id_produto = 1; 
+
+
+----ATUALIZAR TIPO DE ENTREGA----
+
+UPDATE entrega
+SET tipo_entrega = 'Motoboy'
+WHERE id_entrega = 2;
